74976974e6a2a05fcbf9d1755161a69b18a16a4f
Daniel Easterman <daniel.easterman@gmail.com>
2015-02-15 16:37:15 +0000

    tried to make work with vimeo but not responding

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -9,6 +9,8 @@ class Flight < ActiveRecord::Base
   auto_html_for :video_link do
     html_escape
     youtube(:width => 600, :height => 250, :autoplay => false)
+    # Below doesn't do anything.
+    #vimeo(:width => 600, :height => 250, :autoplay => false)
     link :target => "_blank", :rel => "nofollow"
     simple_format
   end
