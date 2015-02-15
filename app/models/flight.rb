7d81bfb9a379e00c1775f0df3d58babab58c179e
Daniel Easterman <daniel.easterman@gmail.com>
2015-02-15 14:53:15 +0000

    deletes old regex code and puts in surplus file

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,15 +1,9 @@
 class Flight < ActiveRecord::Base
-  # before_save :get_video_id
 
   belongs_to :user
   belongs_to :camera
 
   has_many :drones
-
-  # def get_video_id
-  #   regex = /youtube.com.*(?:\/|v=)([^&$]+)/
-  #   self.video_link = self.video_link.match(regex)[1] if self.video_link.match(regex)
-  # end
 
     
 end
