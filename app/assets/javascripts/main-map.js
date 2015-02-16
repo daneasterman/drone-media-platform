74425d9d724867819bb2be08220241ebc9e01e3b
Daniel Easterman <daniel.easterman@gmail.com>
2015-02-16 16:38:09 +0000

    workings on main explore map page, zoomed out to world view

--- app/assets/javascripts/main-map.js
+++ app/assets/javascripts/main-map.js
@@ -3,39 +3,38 @@ var myMap = myMap || {};
 myMap.initialize = function() {
 
   var mapOptions = {
-    center: { lat:  51.52, lng: -0.115},
-    zoom: 14,
-    mapTypeId:google.maps.MapTypeId.ROADMAP //default
+    zoom: 2,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
   };
 
-  myMap.map = new google.maps.Map(myMap.mapCanvas, mapOptions);
+  myMap.map = new google.maps.Map(myMap.mapCanvas, mapOptions); 
 
-  var markerOptions = {
-    position: { lat: 51.53, lng: -0.109446 }
-  };
+    var markerOptions = {
+      position: { lat: 51.53, lng: -0.109446 }
+    };
 
-  var marker = new google.maps.Marker(markerOptions);
-  marker.setMap(myMap.map);
+    var marker = new google.maps.Marker(markerOptions);
+    marker.setMap(myMap.map);
 
-  var popupOptions = {
-    content: "<strong>Arbitrary place location!</strong>"
+    var popupOptions = {
+      content: "<strong>Arbitrary place location!</strong>"
+    }
+
+    var popup = new google.maps.InfoWindow(popupOptions);
+
+    google.maps.event.addListener(marker, 'click', function(){
+      popup.open(myMap.map, marker);
+    });
+
+    setTimeout(function(){
+      myMap.map.setCenter(marker.getPosition());
+    }, 1000);
+
   }
 
-  var popup = new google.maps.InfoWindow(popupOptions);
-
-  google.maps.event.addListener(marker, 'click', function(){
-    popup.open(myMap.map, marker);
+  $(function(){
+    if ($('#map-canvas').length > 0) {
+      myMap.mapCanvas = $('#map-canvas')[0];
+      myMap.initialize();
+    }
   });
-
-  setTimeout(function(){
-    myMap.map.setCenter(marker.getPosition());
-  }, 1000);
-
-}
-
-$(function(){
-  if ($('#map-canvas').length > 0) {
-    myMap.mapCanvas = $('#map-canvas')[0];
-    myMap.initialize();
-  }
-});
