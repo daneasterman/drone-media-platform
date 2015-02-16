5dcb89fc4d0b7e30bea1a4c044a39690bd9b9e02
Daniel Easterman <daniel.easterman@gmail.com>
2015-02-16 19:20:54 +0000

    markers and infowindows displaying on main-map

--- app/assets/javascripts/main-map.js
+++ app/assets/javascripts/main-map.js
@@ -4,37 +4,76 @@ myMap.initialize = function() {
 
   var mapOptions = {
     zoom: 2,
-    mapTypeId:google.maps.MapTypeId.ROADMAP
+    mapTypeId:google.maps.MapTypeId.ROADMAP //default
   };
 
-  myMap.map = new google.maps.Map(myMap.mapCanvas, mapOptions); 
+  myMap.map = new google.maps.Map(myMap.mapCanvas, mapOptions);
 
-    var markerOptions = {
-      position: { lat: 51.53, lng: -0.109446 }
-    };
+  var markerOptions = {
+    position: { lat: 51.53, lng: -0.109446 }
+  };
 
-    var marker = new google.maps.Marker(markerOptions);
-    marker.setMap(myMap.map);
+  var marker = new google.maps.Marker(markerOptions);
+  marker.setMap(myMap.map);
 
-    var popupOptions = {
-      content: "<strong>Arbitrary place location!</strong>"
-    }
+  // var popupOptions = {
+  //   content: "<strong>Arbitrary place location!</strong>"
+  // }
 
-    var popup = new google.maps.InfoWindow(popupOptions);
+  // var popup = new google.maps.InfoWindow(popupOptions);
 
-    google.maps.event.addListener(marker, 'click', function(){
-      popup.open(myMap.map, marker);
-    });
+  myMap.popups = [];
 
-    setTimeout(function(){
-      myMap.map.setCenter(marker.getPosition());
-    }, 1000);
+  google.maps.event.addListener(marker, 'click', function(){
+    popup.open(myMap.map, marker);
+  });
 
+  setTimeout(function(){
+    myMap.map.setCenter(marker.getPosition());
+  }, 1000);
+
+}
+
+$(function(){
+  if ($('#map-canvas').length > 0) {
+    myMap.mapCanvas = $('#map-canvas')[0];
+    myMap.initialize();
   }
 
-  $(function(){
-    if ($('#map-canvas').length > 0) {
-      myMap.mapCanvas = $('#map-canvas')[0];
-      myMap.initialize();
-    }
-  });
+  var data = []
+
+  $.ajax({
+      type: "GET",
+      url: "/flights",
+      data: data,
+      dataType: 'json',
+      success: function(response){
+        console.log(response);
+        renderFlights(response);
+
+      }
+    });
+
+  var renderFlights = function(flights) {
+    flights.forEach(function(flight){
+
+      var marker = new google.maps.Marker({ position: {lat: flight.lat, lng: flight.lng} });
+
+      marker.setMap(myMap.map);
+      marker.popup = new google.maps.InfoWindow({ content: flight.formatted_address }) // content: flight.drone.model_make});
+      myMap.popups.push(marker.popup);
+      setListener(marker);
+    });
+  };  
+
+  function setListener(marker) {    
+    google.maps.event.addListener(marker, 'click', function() {
+      console.log(this);
+      myMap.popups.forEach(function(popup){
+        popup.close();
+      });
+      marker.popup.open(myMap.map, this);
+    });
+  }
+
+});
